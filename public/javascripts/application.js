d167cea7a5bf3f9a5c90d3ec0b80789c0f908e22
Christopher E. Walker <cewalker@uchicago.edu>
2011-08-16 16:47:19 -0500

    changes

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,7 +2,28 @@
 // This file is automatically included by javascript_include_tag :defaults
 
 $(function() {
+// ======================
 
-	console.log('dom loaded');
+
+console.log('dom loaded');
+
+window.Place = Backbone.Model.extend({
 	
+	defaults: {
+		name: 'empty',
+		lat : 'empty',
+		lng : 'empty'
+	},
+	
+	initialize: {
+		
+	}
+});
+
+window.Places = Backbone.Collection.extend({
+	
+});
+
+
+// ======================	
 });
